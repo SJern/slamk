435e125305f94ff494239dd638dc6864402d74d5
veechau <veechau@gmail.com>
2016-07-23 18:26:44 -0700

    Show/hide favorites panel onClick

--- frontend/components/fav-sidebar.jsx
+++ frontend/components/fav-sidebar.jsx
@@ -8,94 +8,35 @@ const MessageStore = require('../stores/message_store');
 
 const Favorites = require('./favorites');
 
-const styling = {
-  root: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: 'hidden',
+const FavoriteSideBar = React.createClass({
+  getInitialState(){
+    return { hidden: true , buttonText: "Show Favorites"};
   },
-  sidebar: {
-    zIndex: 2,
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    transition: 'transform .3s ease-out',
-    WebkitTransition: '-webkit-transform .3s ease-out',
-    willChange: 'transform',
-    overflowY: 'auto',
-  },
-  content: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: 'auto',
-    transition: 'left .3s ease-out, right .3s ease-out',
-  },
-  overlay: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    opacity: 0,
-    visibility: 'hidden',
-    transition: 'opacity .3s ease-out',
-    backgroundColor: 'rgba(0,0,0,.3)',
-  },
-  dragHandle: {
-    zIndex: 1,
-    position: 'fixed',
-    top: 0,
-    bottom: 0,
-  },
-};
-
-var FavoriteSideBar = React.createClass({
-  getInitialState: function() {
-    return {sidebarOpen: false, favorites: []};
-  },
-  componentDidMount(){
-    this.favoriteStoreListener = FavoriteStore.addListener(this._onChange);
-  },
-  componentWillUnmount(){
-    this.favoriteStoreListener.remove();
-  },
-  _onChange(){
-    let currentUser = SessionStore.currentUser();
-    this.setState({favorites: FavoriteStore.findByUser(currentUser.id)});
-  },
-  onSetSidebarOpen: function(open) {
-    this.setState({sidebarOpen: open});
-  },
+  togglePanel(e){
+    e.preventDefault();
+      if (this.state.hidden === true) {
+        this.setState({hidden: false, buttonText: "Hide Favorites"});
+    // $("#nav").stop().animate({marginTop:"-100px"}, 200);
+      } else {
+        this.setState({hidden: true, buttonText: "Show Favorites"});
+    // $("#nav").stop().animate({marginTop:"0px"}, 200);
+      }
+    // this.setState( {hidden: false} );
+},
   render: function() {
-    var sidebarContent = "";
-
-    if (this.props.favorite){
-    sidebarContent = (
-      <ul>
-      {this.props.favorites.forEach( favorite => {
-        return <li>Favorites </li>;
-      })}
-      </ul>
-    );
-  }
-
-    return (
-      <Sidebar styles={styling}
-               sidebar={sidebarContent}
-               open={this.state.sidebarOpen}
-               onSetOpen={this.onSetSidebarOpen}
-               docked={false}
-               transitions={true}
-               pullRight={true}>
+    let content;
+    if (this.state.hidden === true){
+      content = "";
+    } else {
+      content = (
         <Favorites roomId={this.props.roomId}/>
-      </Sidebar>
+      );
+    }
+    return (
+      <div>
+        <button onClick={this.togglePanel}>{this.state.buttonText}</button>
+        {content}
+      </div>
     );
   }
 });
